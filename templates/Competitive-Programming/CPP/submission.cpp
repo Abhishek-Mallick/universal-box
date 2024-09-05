ca4088b564b4c6d8b15fa074d794e17ee605710e
Abhishek Mallick <mallickabhishek97@gmail.com>
2024-09-06 00:01:08 +0530

    fix(templates): Fixed temp/ fir bug

---
+++ templates/Competitive-Programming/CPP/submission.cpp
@@ -0,0 +1,100 @@
+/* Universal-Box*/
+
+#define f first
+#define s second
+#define endl '\n'
+#define sp <<" "<<
+#define pb push_back
+#define MOD 1000000007
+#define fora(a) for(auto u:a)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b)))
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define test int tc; cin>>tc; while(tc--)
+#define forn(i,n) for(auto i=0; i<n; i++)
+#define printv(a) {for(auto u:a) cout<<u<<" "; cout<<endl;}
+#define printm(a) {for(auto u:a) cout<<u.f sp u.s<<endl;}
+#define op() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fori(a,b,c) {for(a = c.begin(); a!=b; a++) cout<< *a<< " "; cout<<endl;}
+#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<int>::iterator vit;
+typedef set<int> si;
+typedef map<int, int> mii;
+const double eps = 1e-9;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* clang-format off */
+
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+/* FUNCTIONS */
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+
+/* UTILS */
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+
+/* clang-format on */
+
+/* Main()  function */
+int main()
+{
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	//freopen("output.txt","w",stdout);
+	#endif
+
+	int tc;
+	tc = read(int);
+
+	while(tc--){
+		write(tc);
+	}
+	return 0;
+}
+/* Main() Ends Here */
