e4bcd45cd42b09a55a26ef5e61edca5eaadde057
Ramya <176913971+Ramya-Clg@users.noreply.github.com>
2024-10-02 21:32:24 +0530

    added react(Frontend) + express(Backend)

---
+++ template/FullStack/React(Frontend)+Express(Backend)/Backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
---
+++ template/FullStack/React(Frontend)+Express(Backend)/Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      Welcome to Your React App
+    </>
+  )
+}
+
+export default App
