51cccc588ba791fc372c1d0be4a7d9079f302248
Akash Jaiswal <akashjaiswal3846@gmail.com>
2024-10-07 01:05:52 +0530

    fix: actual template for react project

--- template/FullStack/React(Frontend)+Express(Backend)/client/src/App.jsx
+++ template/FullStack/React(Frontend)+Express(Backend)/client/src/App.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home' 
-import Signin from './pages/Signin'
-import Signup from './pages/Signup'
-import Header from './components/Header'
-import './index.css';
-import Footer from './components/Footer'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
+
+export default App
--- template/FullStack/React(Frontend)+Express(Backend)/client/src/main.jsx
+++ template/FullStack/React(Frontend)+Express(Backend)/client/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Example from './App.jsx'
+import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Example />
+    <App />
   </StrictMode>,
 )
